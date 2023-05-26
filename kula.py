28d7217b0e112d4f1583003dba51377c2eabbefb
Abr4k4d4br4 <131619715+Abr4k4d4br4@users.noreply.github.com>
2023-05-26 14:08:00 +0200

    Pole oraz objętość kuli

---
+++ kula.py
@@ -0,0 +1,8 @@
+import math
+
+class Kalkulator:
+    def oblicz_pole_kuli(self, promien):
+        return 4 * math.pi * promien ** 2
+
+    def oblicz_objetosc_kuli(self, promien):
+        return (4 / 3) * math.pi * promien ** 3
