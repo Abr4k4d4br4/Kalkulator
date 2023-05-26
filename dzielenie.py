e312c550d3367d4014eef41703f4f59828dc08af
SulekSulowski <126951489+SulekSulowski@users.noreply.github.com>
2023-05-26 12:56:50 +0200

    Add files via upload

---
+++ dzielenie.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Kalkulator:
+    def __init__(self):  # tworzenie pamięci
+        self.memory = []
+
+    def _trim_memory(self):  # pamieć ostatnich dziesięciu zadań
+        if len(self.memory) > 10:
+            self.memory = self.memory[-10:]
+
+    def dzielenie(self, a, b): #dzielenie a przez b
+        if b == 0:
+            return "Dzielenie przez zero!"
+        else:
+            self.memory.append(f"{a} / {b} = {a/b}")
+            self._trim_memory()
+            return a/b
