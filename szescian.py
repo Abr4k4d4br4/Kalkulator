8c463e480dd5626b61bc7ead81151757f3d8ddaf
Abr4k4d4br4 <131619715+Abr4k4d4br4@users.noreply.github.com>
2023-05-27 13:24:35 +0200

    Add files via upload

---
+++ szescian.py
@@ -0,0 +1,9 @@
+import math
+
+class Kalkulator:
+    def oblicz_pole_szescianu(self, dlugosc_boku):
+        return 6 * dlugosc_boku ** 2
+
+    def oblicz_objetosc_szescianu(self, dlugosc_boku):
+        return dlugosc_boku ** 3
+
