9ff1b7b08c3a3caa7f4e0fb8a40f2f5bd27cabf7
SulekSulowski <126951489+SulekSulowski@users.noreply.github.com>
2023-05-29 15:56:59 +0200

    Add files via upload

---
+++ dodawanie_macierz.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Kalkulator:
+    def __init__(self):  # tworzenie pamięci
+        self.memory = []
+
+    def _trim_memory(self):  # pamieć ostatnich dziesięciu zadań
+        if len(self.memory) > 10:
+            self.memory = self.memory[-10:]
+
+    def dodawanie_macierzy(self, a, b):  # dodawanie macierzy
+        if len(a) != len(b) or len(a[0]) != len(b[0]):
+            self.memory.append(f"Macierze {a} i {b} są róznych rozmiarów")
+            self._trim_memory()
+            return "Nie można dodać macierzy o różnych rozmiarach."
+
+        result = []
+        for i in range(len(a)):
+            row = []
+            for j in range(len(a[i])):
+                row.append(a[i][j] + b[i][j])
+            result.append(row)
+        self.memory.append(f"{a} + {b} = {result}")
+        self._trim_memory()
+        return result
